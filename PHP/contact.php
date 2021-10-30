37021d2386f9fdbdcdb9a6239682b8a354d8d3f9
ZeroNoSho <inukuro351@gmail.com>
2021-10-31 02:28:47 +0700

    main

--- PHP/contact.php
+++ PHP/contact.php
@@ -1,6 +1,6 @@
 <?php
     $name = $_POST['name'];
-    $visitor_email = $_POST['email'];
+    $email = $_POST['email'];
     $message = $_POST['message'];
 
 
@@ -9,18 +9,17 @@
     $email_subject = "New Form Submission";
 
     $email_body = "User Name: $name.\n".
-                    "User Email: $visitor_email.\n". 
+                    "User Email: $email.\n". 
                         "User Massage: $message.\n";
 
     $to = "inukuro351@gmail.com";
 
     $headers = "From: $email_from \r\n";
 
-    $headers = "Reply-To: $visitor_email \r\n";
+    $headers = "Reply-To: $email \r\n";
 
     mail($to,$email_subject,$email_body,$headers);
 
     header("Location: contact.html");
-
 
 ?>
